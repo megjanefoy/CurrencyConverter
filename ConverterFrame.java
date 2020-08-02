fc03bd3cbd457d8df93fff868b921dbae232b5c2
Megan Jane Thompson <59883929+megjthom@users.noreply.github.com>
2020-08-02 13:29:20 -0500

    Update ConverterFrame.java

--- ConverterFrame.java
+++ ConverterFrame.java
@@ -13,12 +13,9 @@ import java.awt.Color;
 
 /** 
  * Title: Assignment 7 
- * Semester: COP3337 – Fall 2019
+ * Semester: COP3337 â€“ Fall 2019
  * @author Megan Jane Thompson
  *
- * I affirm that this program is entirely my own work
- * and none of it is the work of any other person.
- * 
  * This program creates the graphical interface 
  * and extends Converter, which calculates the conversion 
  * between U.S. dollars (USD), euros (EUR), and British 
